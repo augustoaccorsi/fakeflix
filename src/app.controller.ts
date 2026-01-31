68905ed8c12a7cef168ea41bb10be137351af265
augustoaccorsi <augusto.acorsi@gmail.com>
2026-01-31 16:13:47 -0300

    Setup database + add some e2e tests

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
 import {
   BadRequestException,
+  Body,
   Controller,
   Get,
   HttpCode,
@@ -8,34 +9,38 @@ import {
   Req,
   UploadedFiles,
   UseInterceptors,
-} from "@nestjs/common";
-import { AppService } from "./app.service";
-import { FileFieldsInterceptor } from "@nestjs/platform-express";
-import { diskStorage } from "multer";
-import { randomUUID } from "crypto";
-import { extname } from "path";
+} from '@nestjs/common';
+import { AppService } from './app.service';
+import { FileFieldsInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { randomUUID } from 'crypto';
+import { extname } from 'path';
+import { PrismaService } from '@src/prisma.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly prismaService: PrismaService,
+  ) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
   }
 
-  @Post("video")
+  @Post('video')
   @HttpCode(HttpStatus.CREATED)
   @UseInterceptors(
     FileFieldsInterceptor(
       [
-        { name: "video", maxCount: 1 },
-        { name: "thumbnail", maxCount: 1 },
+        { name: 'video', maxCount: 1 },
+        { name: 'thumbnail', maxCount: 1 },
       ],
       {
-        dest: "./uploads",
+        dest: './uploads',
         storage: diskStorage({
-          destination: "./uploads",
+          destination: './uploads',
           filename: (_req, file, cb) => {
             return cb(
               null,
@@ -44,10 +49,10 @@ export class AppController {
           },
         }),
         fileFilter: (_req, file, cb) => {
-          if (file.mimetype !== "video/mp4" && file.mimetype !== "image/jpeg") {
+          if (file.mimetype !== 'video/mp4' && file.mimetype !== 'image/jpeg') {
             return cb(
               new BadRequestException(
-                "Invalid file type. Only video/mp4 and image/jpeg are supported.",
+                'Invalid file type. Only video/mp4 and image/jpeg are supported.',
               ),
               false,
             );
@@ -59,10 +64,35 @@ export class AppController {
   )
   async uploadVideo(
     @Req() _req: Request,
+    @Body()
+    contentData: {
+      title: string;
+      description: string;
+    },
     @UploadedFiles()
     files: { video?: Express.Multer.File[]; thumbnail?: Express.Multer.File[] },
-  ): Promise<string> {
-    console.log(files);
-    return "video uploaded";
+  ): Promise<any> {
+    const videoFile = files.video?.[0];
+    const thumbnailFile = files.thumbnail?.[0];
+
+    if (!videoFile || !thumbnailFile) {
+      throw new BadRequestException(
+        'Both video and thumbnail files are required.',
+      );
+    }
+
+    return await this.prismaService.video.create({
+      data: {
+        id: randomUUID(),
+        title: contentData.title,
+        description: contentData.description,
+        url: videoFile.path,
+        thumbnailUrl: thumbnailFile.path,
+        sizeInKb: videoFile.size,
+        duration: 100,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    });
   }
 }
